067c934bf094433565b006a790e4d6c798688a8e
hezirel <80560775+hezirel@users.noreply.github.com>
2022-05-12 21:04:46 +0200

    WIP setup

--- src/index.js
+++ src/index.js
@@ -1,23 +1,26 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import './index.css';
+import React from "react";
+import { HashRouter, Routes, Route } from "react-router-dom";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
+import App from "./App";
+import Login from "./components/login/Login";
+import Profile from "./components/profile/Profile";
+import "./index.css";
 
-const container = document.getElementById('root');
+const container = document.getElementById("root");
 const root = createRoot(container);
 
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
+	<React.StrictMode>
+		<Provider store={store}>
+			<HashRouter>
+				<Routes>
+					<Route path="/" element={<App />} />
+					<Route path="/login" element={<Login />}/>
+					<Route path="/profile" element={<Profile />}/>
+				</Routes>
+			</HashRouter>
+		</Provider>
+	</React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
